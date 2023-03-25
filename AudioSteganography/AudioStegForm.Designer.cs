8a8e47c66226c1c867fd9c4d4a5f8f1730a8f41a
QlirimMurati <qlirimmurati@gmail.com>
2023-03-25 23:19:16 +0100

    feat: Changes form utilities naming and adding functionality

--- AudioSteganography/AudioStegForm.Designer.cs
+++ AudioSteganography/AudioStegForm.Designer.cs
@@ -30,18 +30,22 @@
         {
             this.decodeTab = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.lblSecretKeyEncode = new System.Windows.Forms.Label();
+            this.txtSecretKeyEncode = new System.Windows.Forms.TextBox();
             this.lblTextToEncode = new System.Windows.Forms.Label();
             this.lblAudioFileEncode = new System.Windows.Forms.Label();
-            this.txtTextFilePath = new System.Windows.Forms.TextBox();
-            this.btnUploadTextFile = new System.Windows.Forms.Button();
+            this.txtTextFilePathEncode = new System.Windows.Forms.TextBox();
+            this.btnUploadTextFileEncode = new System.Windows.Forms.Button();
             this.btnEncode = new System.Windows.Forms.Button();
-            this.radioTextFile = new System.Windows.Forms.RadioButton();
-            this.radioText = new System.Windows.Forms.RadioButton();
+            this.radioTextFileEncode = new System.Windows.Forms.RadioButton();
+            this.radioTextEncode = new System.Windows.Forms.RadioButton();
             this.txtAreaEncode = new System.Windows.Forms.TextBox();
             this.txtAudioPathEncode = new System.Windows.Forms.TextBox();
             this.progressEncode = new System.Windows.Forms.ProgressBar();
             this.btnUploadAudioEncode = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lblSecretKeyDecode = new System.Windows.Forms.Label();
+            this.txtSecretKeyDecode = new System.Windows.Forms.TextBox();
             this.lblDecodedText = new System.Windows.Forms.Label();
             this.lblAudioFileDecode = new System.Windows.Forms.Label();
             this.btnDecode = new System.Windows.Forms.Button();
@@ -49,10 +53,6 @@
             this.progressDecode = new System.Windows.Forms.ProgressBar();
             this.txtAudioPathDecode = new System.Windows.Forms.TextBox();
             this.btnUploadAudioDecode = new System.Windows.Forms.Button();
-            this.lblSecretKey = new System.Windows.Forms.Label();
-            this.txtAreaSecretKey = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.decodeTab.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -65,20 +65,20 @@
             this.decodeTab.Location = new System.Drawing.Point(-1, 2);
             this.decodeTab.Name = "decodeTab";
             this.decodeTab.SelectedIndex = 0;
-            this.decodeTab.Size = new System.Drawing.Size(429, 499);
+            this.decodeTab.Size = new System.Drawing.Size(429, 465);
             this.decodeTab.TabIndex = 0;
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.lblSecretKey);
-            this.tabPage1.Controls.Add(this.txtAreaSecretKey);
+            this.tabPage1.Controls.Add(this.lblSecretKeyEncode);
+            this.tabPage1.Controls.Add(this.txtSecretKeyEncode);
             this.tabPage1.Controls.Add(this.lblTextToEncode);
             this.tabPage1.Controls.Add(this.lblAudioFileEncode);
-            this.tabPage1.Controls.Add(this.txtTextFilePath);
-            this.tabPage1.Controls.Add(this.btnUploadTextFile);
+            this.tabPage1.Controls.Add(this.txtTextFilePathEncode);
+            this.tabPage1.Controls.Add(this.btnUploadTextFileEncode);
             this.tabPage1.Controls.Add(this.btnEncode);
-            this.tabPage1.Controls.Add(this.radioTextFile);
-            this.tabPage1.Controls.Add(this.radioText);
+            this.tabPage1.Controls.Add(this.radioTextFileEncode);
+            this.tabPage1.Controls.Add(this.radioTextEncode);
             this.tabPage1.Controls.Add(this.txtAreaEncode);
             this.tabPage1.Controls.Add(this.txtAudioPathEncode);
             this.tabPage1.Controls.Add(this.progressEncode);
@@ -86,10 +86,27 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 24);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(421, 471);
+            this.tabPage1.Size = new System.Drawing.Size(421, 437);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Encode";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // lblSecretKeyEncode
+            // 
+            this.lblSecretKeyEncode.AutoSize = true;
+            this.lblSecretKeyEncode.Location = new System.Drawing.Point(9, 309);
+            this.lblSecretKeyEncode.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblSecretKeyEncode.Name = "lblSecretKeyEncode";
+            this.lblSecretKeyEncode.Size = new System.Drawing.Size(60, 15);
+            this.lblSecretKeyEncode.TabIndex = 12;
+            this.lblSecretKeyEncode.Text = "Secret key";
+            // 
+            // txtSecretKeyEncode
+            // 
+            this.txtSecretKeyEncode.Location = new System.Drawing.Point(8, 327);
+            this.txtSecretKeyEncode.Name = "txtSecretKeyEncode";
+            this.txtSecretKeyEncode.Size = new System.Drawing.Size(402, 23);
+            this.txtSecretKeyEncode.TabIndex = 11;
             // 
             // lblTextToEncode
             // 
@@ -111,60 +128,63 @@
             this.lblAudioFileEncode.TabIndex = 9;
             this.lblAudioFileEncode.Text = "Audio file";
             // 
-            // txtTextFilePath
+            // txtTextFilePathEncode
             // 
-            this.txtTextFilePath.Location = new System.Drawing.Point(9, 85);
-            this.txtTextFilePath.Multiline = true;
-            this.txtTextFilePath.Name = "txtTextFilePath";
-            this.txtTextFilePath.Size = new System.Drawing.Size(285, 28);
-            this.txtTextFilePath.TabIndex = 8;
+            this.txtTextFilePathEncode.Location = new System.Drawing.Point(9, 85);
+            this.txtTextFilePathEncode.Multiline = true;
+            this.txtTextFilePathEncode.Name = "txtTextFilePathEncode";
+            this.txtTextFilePathEncode.Size = new System.Drawing.Size(285, 28);
+            this.txtTextFilePathEncode.TabIndex = 8;
             // 
-            // btnUploadTextFile
+            // btnUploadTextFileEncode
             // 
-            this.btnUploadTextFile.Location = new System.Drawing.Point(298, 85);
-            this.btnUploadTextFile.Name = "btnUploadTextFile";
-            this.btnUploadTextFile.Size = new System.Drawing.Size(112, 28);
-            this.btnUploadTextFile.TabIndex = 7;
-            this.btnUploadTextFile.Text = "Text file";
-            this.btnUploadTextFile.UseVisualStyleBackColor = true;
+            this.btnUploadTextFileEncode.Location = new System.Drawing.Point(298, 85);
+            this.btnUploadTextFileEncode.Name = "btnUploadTextFileEncode";
+            this.btnUploadTextFileEncode.Size = new System.Drawing.Size(112, 28);
+            this.btnUploadTextFileEncode.TabIndex = 7;
+            this.btnUploadTextFileEncode.Text = "Text file";
+            this.btnUploadTextFileEncode.UseVisualStyleBackColor = true;
+            this.btnUploadTextFileEncode.Click += new System.EventHandler(this.btnUploadTextFile_Click);
             // 
             // btnEncode
             // 
-            this.btnEncode.Location = new System.Drawing.Point(299, 437);
+            this.btnEncode.Location = new System.Drawing.Point(299, 403);
             this.btnEncode.Name = "btnEncode";
             this.btnEncode.Size = new System.Drawing.Size(112, 28);
             this.btnEncode.TabIndex = 6;
             this.btnEncode.Text = "Encode";
             this.btnEncode.UseVisualStyleBackColor = true;
             // 
-            // radioTextFile
+            // radioTextFileEncode
             // 
-            this.radioTextFile.AutoSize = true;
-            this.radioTextFile.Location = new System.Drawing.Point(10, 58);
-            this.radioTextFile.Name = "radioTextFile";
-            this.radioTextFile.Size = new System.Drawing.Size(62, 19);
-            this.radioTextFile.TabIndex = 5;
-            this.radioTextFile.TabStop = true;
-            this.radioTextFile.Text = "Textfile";
-            this.radioTextFile.UseVisualStyleBackColor = true;
+            this.radioTextFileEncode.AutoSize = true;
+            this.radioTextFileEncode.Location = new System.Drawing.Point(10, 58);
+            this.radioTextFileEncode.Name = "radioTextFileEncode";
+            this.radioTextFileEncode.Size = new System.Drawing.Size(62, 19);
+            this.radioTextFileEncode.TabIndex = 5;
+            this.radioTextFileEncode.TabStop = true;
+            this.radioTextFileEncode.Text = "Textfile";
+            this.radioTextFileEncode.UseVisualStyleBackColor = true;
+            this.radioTextFileEncode.CheckedChanged += new System.EventHandler(this.radioEncodingType_CheckedChanged);
             // 
-            // radioText
+            // radioTextEncode
             // 
-            this.radioText.AutoSize = true;
-            this.radioText.Location = new System.Drawing.Point(76, 58);
-            this.radioText.Name = "radioText";
-            this.radioText.Size = new System.Drawing.Size(46, 19);
-            this.radioText.TabIndex = 4;
-            this.radioText.TabStop = true;
-            this.radioText.Text = "Text";
-            this.radioText.UseVisualStyleBackColor = true;
+            this.radioTextEncode.AutoSize = true;
+            this.radioTextEncode.Location = new System.Drawing.Point(76, 58);
+            this.radioTextEncode.Name = "radioTextEncode";
+            this.radioTextEncode.Size = new System.Drawing.Size(46, 19);
+            this.radioTextEncode.TabIndex = 4;
+            this.radioTextEncode.TabStop = true;
+            this.radioTextEncode.Text = "Text";
+            this.radioTextEncode.UseVisualStyleBackColor = true;
+            this.radioTextEncode.CheckedChanged += new System.EventHandler(this.radioEncodingType_CheckedChanged);
             // 
             // txtAreaEncode
             // 
             this.txtAreaEncode.Location = new System.Drawing.Point(9, 143);
             this.txtAreaEncode.Multiline = true;
             this.txtAreaEncode.Name = "txtAreaEncode";
-            this.txtAreaEncode.Size = new System.Drawing.Size(402, 189);
+            this.txtAreaEncode.Size = new System.Drawing.Size(402, 150);
             this.txtAreaEncode.TabIndex = 3;
             // 
             // txtAudioPathEncode
@@ -177,7 +197,7 @@
             // 
             // progressEncode
             // 
-            this.progressEncode.Location = new System.Drawing.Point(9, 400);
+            this.progressEncode.Location = new System.Drawing.Point(8, 365);
             this.progressEncode.Name = "progressEncode";
             this.progressEncode.Size = new System.Drawing.Size(401, 20);
             this.progressEncode.TabIndex = 1;
@@ -190,11 +210,12 @@
             this.btnUploadAudioEncode.TabIndex = 0;
             this.btnUploadAudioEncode.Text = "Upload audio";
             this.btnUploadAudioEncode.UseVisualStyleBackColor = true;
+            this.btnUploadAudioEncode.Click += new System.EventHandler(this.btnUploadAudioEncode_Click);
             // 
             // tabPage2
             // 
-            this.tabPage2.Controls.Add(this.label1);
-            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.lblSecretKeyDecode);
+            this.tabPage2.Controls.Add(this.txtSecretKeyDecode);
             this.tabPage2.Controls.Add(this.lblDecodedText);
             this.tabPage2.Controls.Add(this.lblAudioFileDecode);
             this.tabPage2.Controls.Add(this.btnDecode);
@@ -205,10 +226,27 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 24);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(421, 471);
+            this.tabPage2.Size = new System.Drawing.Size(421, 437);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Decode";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // lblSecretKeyDecode
+            // 
+            this.lblSecretKeyDecode.AutoSize = true;
+            this.lblSecretKeyDecode.Location = new System.Drawing.Point(10, 60);
+            this.lblSecretKeyDecode.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblSecretKeyDecode.Name = "lblSecretKeyDecode";
+            this.lblSecretKeyDecode.Size = new System.Drawing.Size(60, 15);
+            this.lblSecretKeyDecode.TabIndex = 14;
+            this.lblSecretKeyDecode.Text = "Secret key";
+            // 
+            // txtSecretKeyDecode
+            // 
+            this.txtSecretKeyDecode.Location = new System.Drawing.Point(9, 78);
+            this.txtSecretKeyDecode.Name = "txtSecretKeyDecode";
+            this.txtSecretKeyDecode.Size = new System.Drawing.Size(402, 23);
+            this.txtSecretKeyDecode.TabIndex = 13;
             // 
             // lblDecodedText
             // 
@@ -232,7 +270,7 @@
             // 
             // btnDecode
             // 
-            this.btnDecode.Location = new System.Drawing.Point(300, 434);
+            this.btnDecode.Location = new System.Drawing.Point(297, 403);
             this.btnDecode.Name = "btnDecode";
             this.btnDecode.Size = new System.Drawing.Size(112, 28);
             this.btnDecode.TabIndex = 7;
@@ -244,12 +282,12 @@
             this.txtAreaDecode.Location = new System.Drawing.Point(9, 133);
             this.txtAreaDecode.Multiline = true;
             this.txtAreaDecode.Name = "txtAreaDecode";
-            this.txtAreaDecode.Size = new System.Drawing.Size(402, 246);
+            this.txtAreaDecode.Size = new System.Drawing.Size(402, 215);
             this.txtAreaDecode.TabIndex = 6;
             // 
             // progressDecode
             // 
-            this.progressDecode.Location = new System.Drawing.Point(10, 397);
+            this.progressDecode.Location = new System.Drawing.Point(9, 369);
             this.progressDecode.Name = "progressDecode";
             this.progressDecode.Size = new System.Drawing.Size(400, 20);
             this.progressDecode.TabIndex = 5;
@@ -271,47 +309,13 @@
             this.btnUploadAudioDecode.Text = "Upload audio";
             this.btnUploadAudioDecode.UseVisualStyleBackColor = true;
             // 
-            // lblSecretKey
-            // 
-            this.lblSecretKey.AutoSize = true;
-            this.lblSecretKey.Location = new System.Drawing.Point(10, 344);
-            this.lblSecretKey.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblSecretKey.Name = "lblSecretKey";
-            this.lblSecretKey.Size = new System.Drawing.Size(60, 15);
-            this.lblSecretKey.TabIndex = 12;
-            this.lblSecretKey.Text = "Secret key";
-            // 
-            // txtAreaSecretKey
-            // 
-            this.txtAreaSecretKey.Location = new System.Drawing.Point(9, 362);
-            this.txtAreaSecretKey.Name = "txtAreaSecretKey";
-            this.txtAreaSecretKey.Size = new System.Drawing.Size(402, 23);
-            this.txtAreaSecretKey.TabIndex = 11;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 60);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 15);
-            this.label1.TabIndex = 14;
-            this.label1.Text = "Secret key";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(9, 78);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(402, 23);
-            this.textBox1.TabIndex = 13;
-            // 
-            // Form1
+            // AudioStegForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(428, 500);
+            this.ClientSize = new System.Drawing.Size(428, 465);
             this.Controls.Add(this.decodeTab);
-            this.Name = "Form1";
+            this.Name = "AudioStegForm";
             this.Text = "Form1";
             this.decodeTab.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
@@ -326,11 +330,11 @@
 
         private TabControl decodeTab;
         private TabPage tabPage1;
-        private TextBox txtTextFilePath;
-        private Button btnUploadTextFile;
+        private TextBox txtTextFilePathEncode;
+        private Button btnUploadTextFileEncode;
         private Button btnEncode;
-        private RadioButton radioTextFile;
-        private RadioButton radioText;
+        private RadioButton radioTextFileEncode;
+        private RadioButton radioTextEncode;
         private TextBox txtAreaEncode;
         private TextBox txtAudioPathEncode;
         private ProgressBar progressEncode;
@@ -345,9 +349,9 @@
         private Label lblAudioFileEncode;
         private Label lblDecodedText;
         private Label lblAudioFileDecode;
-        private Label lblSecretKey;
-        private TextBox txtAreaSecretKey;
-        private Label label1;
-        private TextBox textBox1;
+        private Label lblSecretKeyEncode;
+        private TextBox txtSecretKeyEncode;
+        private Label lblSecretKeyDecode;
+        private TextBox txtSecretKeyDecode;
     }
 }
--- AudioSteganography/AudioStegForm.cs
+++ AudioSteganography/AudioStegForm.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace AudioSteganography
 {
     public partial class AudioStegForm : Form
@@ -5,6 +7,56 @@ namespace AudioSteganography
         public AudioStegForm()
         {
             InitializeComponent();
+            main();
+        }
+
+        private void main()
+        {
+            radioTextFileEncode.Checked = true;
+            txtAreaEncode.Enabled = false;
+        }
+
+        private void btnUploadAudioEncode_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Wave Audio Files (*.wav)|*.wav";
+            dialog.Title = "Select a Wave Audio File";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                txtAudioPathEncode.Text = dialog.FileName;
+            }
+        }
+
+        private void btnUploadTextFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Text Files (.txt)|*.txt";
+            dialog.Title = "Select a Text File";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                txtTextFilePathEncode.Text = dialog.FileName;
+            }
+        }
+
+        private void radioEncodingType_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton encodingChoice = (RadioButton)sender;
+
+            if(encodingChoice.Checked)
+            {
+                if(encodingChoice.Text == "Text")
+                {
+                    txtAreaEncode.Enabled = true;
+                    txtTextFilePathEncode.Enabled = false;
+                    btnUploadTextFileEncode.Enabled = false;
+                }
+                else
+                {
+                    txtTextFilePathEncode.Enabled = true;
+                    btnUploadTextFileEncode.Enabled = true;
+                    txtAreaEncode.Enabled = false;
+                }
+            }
         }
     }
 }
